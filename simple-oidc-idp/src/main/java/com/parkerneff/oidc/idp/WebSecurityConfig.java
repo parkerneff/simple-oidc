661923df8a3aa37debb33f03b2d0b26ac8fcd7ff
pneff <pneff@unicon.net>
2018-01-25 14:48:00 -0700

    working with custom user details

--- simple-oidc-idp/src/main/java/com/parkerneff/oidc/idp/WebSecurityConfig.java
+++ simple-oidc-idp/src/main/java/com/parkerneff/oidc/idp/WebSecurityConfig.java
@@ -1,15 +1,10 @@
 package com.parkerneff.oidc.idp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
@@ -35,17 +30,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(new MyAuthenticationProvider());
     }
 
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("user").password("password").roles("USER");
-//    }
-
-
-//    @Bean
-//    public PasswordEncoder encoder() {
-//        AuthenticationProvider provide = new
-//        return new BCryptPasswordEncoder(11);
-//    }
+//
 }
